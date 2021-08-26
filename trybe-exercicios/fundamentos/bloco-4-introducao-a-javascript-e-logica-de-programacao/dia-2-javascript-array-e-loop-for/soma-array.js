10156a028f3370e53f9ae3b577e31e758185fd1f
Gabriel Oliveira <gabrielfrox@gmail.com>
2021-08-26 16:03:53 -0300

    JS de media aritmetica

--- trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/soma-array.js
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/soma-array.js
@@ -1,6 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 var soma = 0
-for (index = 0; index < numbers.length; index += 1) {
+for (var index = 0; index < numbers.length; index += 1) {
   soma = soma + numbers[index]
 }
 console.log(soma)
